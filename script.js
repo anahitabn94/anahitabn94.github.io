5736238efa699f41dc43debb45dada1962fb8c4c
Anahita <79853151+anahitabn94@users.noreply.github.com>
2026-01-18 14:01:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// const toggleBtn = document.getElementById('tabs-toggle');
+// const tabs = document.getElementById('Tabs');
+// toggleBtn.addEventListener('click', () => {
+//     tabs.classList.toggle('show');
+// });
+// // Optional: Hide menu when clicking outside
+// document.addEventListener('click', (e) => {
+//     if (
+//     window.innerWidth <= 800 &&
+//     !tabs.contains(e.target) &&
+//     e.target !== toggleBtn
+//     ) {
+//     tabs.classList.remove('show');
+//     }
+// });
